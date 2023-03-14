ffa487a9f5616d3d17b80937776c504a0071e000
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-14 13:41:00 +0100

    modified bot.go

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -2,12 +2,8 @@ package bot
 
 import (
 	"context"
-	api "subscription-bot/pkg/api"
 	config "subscription-bot/pkg/config"
-	database "subscription-bot/pkg/database"
-	"time"
 
-	"github.com/go-co-op/gocron"
 	"go.mongodb.org/mongo-driver/mongo"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -74,66 +70,5 @@ func (b *Bot) Start() error {
 			}
 		}
 	}
-	return nil
-}
-
-func (b *Bot) initScheduler() {
-	ns := gocron.NewScheduler(time.UTC)
-
-	ns.Every(1).Hour().Do(func() {
-		b.sendScheduledMessage()
-	})
-	ns.StartAsync()
-}
-
-func (b *Bot) sendScheduledMessage() error {
-	subs, err := database.GetAllSubscribers(b.client)
-	if err != nil {
-		log.Errorf("error getting all subscribres: %s", err)
-		return err
-	}
-
-	for _, sub := range subs {
-		s := sub
-
-		userLocation, err := time.LoadLocation(s.Timezone)
-		if err != nil {
-			log.Errorf("error loading location: %s", err)
-			return err
-		}
-
-		ns := gocron.NewScheduler(userLocation)
-
-		userTime, err := time.Parse("15:04", s.Time)
-		if err != nil {
-			log.Errorf("error parsing user time from mongoDB: %s", err)
-			return err
-		}
-
-		ns.Every(1).Day().At(userTime).Do(func() {
-			if err := b.sendWeatherMessage(s.Latitude, s.Longitude, s.ChatID); err != nil {
-				return
-			}
-		})
-		ns.StartAsync()
-	}
-
-	return nil
-}
-
-func (b *Bot) sendWeatherMessage(lat float64, long float64, chatID int64) error {
-	weatherMsg, _, err := api.RequestWeatherForecast(lat, long, b.weatherToken)
-	if err != nil {
-		log.Errorf("error sending weather forecast message: %s", err)
-		return err
-	}
-
-	msg := tgbotapi.NewMessage(chatID, weatherMsg)
-
-	if _, err = b.bot.Send(msg); err != nil {
-		log.Errorf("error sending message to telegram: %s ", err)
-		return err
-	}
-
 	return nil
 }
