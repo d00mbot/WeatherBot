1a88d4c630920469c22211fa8df616513780a3c8
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-16 12:18:24 +0100

    modified main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,18 +4,14 @@ import (
 	b "subscription-bot/pkg/bot"
 	config "subscription-bot/pkg/config"
 	database "subscription-bot/pkg/database"
+	_ "time/tzdata"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	cfg, err := config.LoadENV("app.env")
-	if err != nil {
-		return
-	}
-
-	responses, err := config.InitResponses()
+	cfg, err := config.Init()
 	if err != nil {
 		return
 	}
@@ -33,7 +29,7 @@ func main() {
 		return
 	}
 
-	bot := b.Newbot(botApi, client, cfg.WeatherToken, responses.Responses)
+	bot := b.Newbot(botApi, client, cfg.WeatherToken, cfg.Responses)
 
 	if err := bot.Start(); err != nil {
 		log.Errorf("unable to start bot: %s", err)
