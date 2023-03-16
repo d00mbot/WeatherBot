6a11a9df9e4f8e6bd3c6f77a3742f992b4951624
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-16 18:38:36 +0100

    modified subscriber

--- pkg/domain/subscriber.go
+++ pkg/domain/subscriber.go
@@ -1,14 +1,9 @@
 package domain
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type Subscriber struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	ChatID    int64              `bson:"chatid"`
-	Latitude  float64            `bson:"latitude"`
-	Longitude float64            `bson:"longitude"`
-	Timezone  string             `bson:"timezone"`
-	Time      string             `bson:"time"`
+	ChatID    int64   `bson:"chatid"`
+	Latitude  float64 `bson:"latitude"`
+	Longitude float64 `bson:"longitude"`
+	Timezone  string  `bson:"timezone"`
+	Time      string  `bson:"time"`
 }
