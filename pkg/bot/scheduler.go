d493a5eb9c32360b7f57cf1dc8cccf2e9a96b8ad
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-31 14:52:49 +0200

    modified scheduler

--- pkg/bot/scheduler.go
+++ pkg/bot/scheduler.go
@@ -1,14 +1,13 @@
 package bot
 
 import (
-	api "subscription-bot/pkg/api"
-	database "subscription-bot/pkg/database"
 	"time"
 
 	"github.com/go-co-op/gocron"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	log "github.com/sirupsen/logrus"
 )
+
+const timeLayout = "15:04"
 
 func (b *Bot) initScheduler() {
 	ns := gocron.NewScheduler(time.UTC)
@@ -20,31 +19,33 @@ func (b *Bot) initScheduler() {
 }
 
 func (b *Bot) sendScheduledMessage() error {
-	subs, err := database.GetAllSubscribers(b.client)
+	logger := b.container.GetLogger()
+
+	users, err := b.storage.FindAll(b.client)
 	if err != nil {
-		log.Errorf("error getting all subscribres: %s", err)
+		logger.Errorf("error getting all users: %s", err)
 		return err
 	}
 
-	for _, sub := range subs {
-		s := sub
+	for _, user := range users {
+		u := user
 
-		userLocation, err := time.LoadLocation(s.Timezone)
+		userLocation, err := time.LoadLocation(u.Timezone)
 		if err != nil {
-			log.Errorf("error loading location: %s", err)
+			logger.Errorf("error loading location: %s", err)
 			return err
 		}
 
 		ns := gocron.NewScheduler(userLocation)
 
-		userTime, err := time.Parse("15:04", s.Time)
+		userTime, err := time.Parse(timeLayout, u.Time)
 		if err != nil {
-			log.Errorf("error parsing user time from mongoDB: %s", err)
+			logger.Errorf("error parsing time: %s", err)
 			return err
 		}
 
 		ns.Every(1).Day().At(userTime).Do(func() {
-			if err := b.sendWeatherMessage(s.Latitude, s.Longitude, s.ChatID); err != nil {
+			if err := b.sendWeatherMessage(u.Latitude, u.Longitude, u.ChatID); err != nil {
 				return
 			}
 		})
@@ -54,17 +55,19 @@ func (b *Bot) sendScheduledMessage() error {
 	return nil
 }
 
-func (b *Bot) sendWeatherMessage(lat float64, long float64, chatID int64) error {
-	weatherMsg, _, err := api.RequestWeatherForecast(lat, long, b.weatherToken)
+func (b *Bot) sendWeatherMessage(lat float64, lon float64, chatID int64) error {
+	logger := b.container.GetLogger()
+
+	weatherMsg, _, err := b.request.RequestWeather(lat, lon, b.container.GetConfig())
 	if err != nil {
-		log.Errorf("error sending weather forecast message: %s", err)
+		logger.Errorf("error creating weather forecast message: %s", err)
 		return err
 	}
 
 	msg := tgbotapi.NewMessage(chatID, weatherMsg)
 
 	if _, err = b.bot.Send(msg); err != nil {
-		log.Errorf("error sending message to telegram: %s ", err)
+		logger.Errorf("error sending message to telegram: %s ", err)
 		return err
 	}
 
