35310ff67777e35fe7b882f3cf9bb38a7afc6d82
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-31 15:02:12 +0200

    renamed to user.go, replaced to models package

--- pkg/domain/subscriber.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Subscriber struct {
-	ChatID    int64   `bson:"chatid"`
-	Latitude  float64 `bson:"latitude"`
-	Longitude float64 `bson:"longitude"`
-	Timezone  string  `bson:"timezone"`
-	Time      string  `bson:"time"`
-}
