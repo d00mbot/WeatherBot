41e0891922823c6cb264d0c3a6b02070b7d8095d
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-31 14:50:09 +0200

    modified main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,38 +1,62 @@
 package main
 
 import (
+	"subscription-bot/pkg/api"
 	b "subscription-bot/pkg/bot"
-	config "subscription-bot/pkg/config"
-	database "subscription-bot/pkg/database"
+	"subscription-bot/pkg/config"
+	"subscription-bot/pkg/container"
+	"subscription-bot/pkg/database"
+	lg "subscription-bot/pkg/logger"
 	_ "time/tzdata"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	cfg, err := config.Init()
+	log, err := lg.NewLogger()
 	if err != nil {
 		return
 	}
 
+	cfg, err := config.LoadConfig(log)
+	if err != nil {
+		log.Errorf("failed to load config: %s", err)
+		return
+	}
+
+	logger, err := lg.LoadLogger(cfg)
+	if err != nil {
+		log.Errorf("faild to load logger: %s", err)
+		return
+	}
+
+	container := container.NewBotContainer(cfg, logger)
+
+	weatherStorage := api.NewWeatherStorageService(container)
+
+	requestService := api.NewRequestWeatherService(weatherStorage)
+
+	mongoStorage := database.NewMongoStorageService(container, requestService)
+
+	userStorage := database.NewUserStorageService(mongoStorage)
+
 	botApi, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
 	if err != nil {
-		log.Errorf("error creating new BotAPI instance: %s", err)
+		logger.Errorf("error creating new BotAPI instance: %s", err)
 		return
 	}
 	botApi.Debug = true
 
-	client, err := database.InitMongoClient(cfg.MongoURI)
+	client, err := userStorage.InitClient(cfg)
 	if err != nil {
-		log.Errorf("error creating mongoDB client: %s", err)
+		logger.Errorf("error creating mongoDB client: %s", err)
 		return
 	}
 
-	bot := b.Newbot(botApi, client, cfg.WeatherToken, cfg.Responses)
+	bot := b.Newbot(botApi, userStorage, client, cfg.Response, container, requestService)
 
 	if err := bot.Start(); err != nil {
-		log.Errorf("unable to start bot: %s", err)
+		logger.Errorf("unable to start bot: %s", err)
 		return
 	}
 }
