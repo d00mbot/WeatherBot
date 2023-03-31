d20d168fa43c12c9170a4ecafbad6cf1ff9daa4e
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-31 14:51:20 +0200

    modified weather

--- pkg/api/weather.go
+++ pkg/api/weather.go
@@ -2,13 +2,23 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
+	"subscription-bot/pkg/config"
+	"subscription-bot/pkg/container"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	log "github.com/sirupsen/logrus"
 )
+
+type WeatherStorageService struct {
+	container container.BotContainer
+}
+
+func NewWeatherStorageService(c container.BotContainer) WeatherStorageService {
+	return WeatherStorageService{container: c}
+}
 
 type Weather struct {
 	TimeZone string `json:"timezone"`
@@ -33,29 +43,43 @@ type Weather struct {
 	} `json:"daily"`
 }
 
-func RequestWeatherForecast(lat float64, long float64, token string) (string, string, error) {
+func (w *WeatherStorageService) requestWeatherForecast(lat float64, lon float64, cfg *config.Config) (string, string, error) {
+	logger := w.container.GetLogger()
+
+	if lat == 0.0 {
+		err := errors.New("latitude is empty")
+		logger.Errorf("faild to request weather forecast: %s", err)
+		return "", "", err
+	}
+
+	if lon == 0.0 {
+		err := errors.New("longitude is empty")
+		logger.Errorf("faild to request weather forecast: %s", err)
+		return "", "", err
+	}
+
 	response, err := http.Get(fmt.Sprintf("https://api.openweathermap.org/data/3.0/onecall?lat=%f&lon=%f&exclude=current,minutely,hourly,alerts&units=metric&appid=%s",
 		lat,
-		long,
-		token))
+		lon,
+		cfg.WeatherToken))
 	if err != nil {
-		log.Errorf("error response weather api %s", err.Error())
+		logger.Errorf("error getting weather api response: %s", err)
 		return "", "", err
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		log.Errorf("error parsing weather api answer %s", err.Error())
+		logger.Errorf("error reading weather api response: %s", err)
 		return "", "", err
 	}
 
 	var resp Weather
 
-	jsonErr := json.Unmarshal(body, &resp)
-	if jsonErr != nil {
-		log.Errorf("error unmarshaling weather answer: %s", jsonErr)
-		return "", "", jsonErr
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		logger.Errorf("error unmarshaling weather api response: %s", err)
+		return "", "", err
 	}
 
 	return string(fmt.Sprintf("Weather forecast for the day:\nTemperature: min %dC, max %dC, Day %dC, Night %dC\nFeels like: Day %dC, Night %dC\nHumidity: %d%%\nCloudiness: %d%%\nWind speed: %.2fm/s\nWeather: %s(%s)",
@@ -64,10 +88,12 @@ func RequestWeatherForecast(lat float64, long float64, token string) (string, st
 		resp.Daily[0].Weather[0].Main, resp.Daily[0].Weather[0].Description)), resp.TimeZone, nil
 }
 
-func GetUserTimezone(message *tgbotapi.Message, token string) (string, error) {
-	_, timeZone, err := RequestWeatherForecast(message.Location.Latitude, message.Location.Longitude, token)
+func (w *WeatherStorageService) requestUserTimezone(message *tgbotapi.Message, cfg *config.Config) (string, error) {
+	logger := w.container.GetLogger()
+
+	_, timeZone, err := w.requestWeatherForecast(message.Location.Latitude, message.Location.Longitude, cfg)
 	if err != nil {
-		log.Errorf("error getting user timezone: %s", err)
+		logger.Errorf("error getting user timezone: %s", err)
 		return "", err
 	}
 	return timeZone, nil
