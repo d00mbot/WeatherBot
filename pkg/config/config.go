a7c1d25d895bad2bfe5d9b75dd1ea86ac278357f
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-03-31 14:54:04 +0200

    modified config.go

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,92 +1,56 @@
 package config
 
 import (
+	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 
-	log "github.com/sirupsen/logrus"
+	"subscription-bot/pkg/domain/models"
 )
 
-type Responses struct {
-	Start          string `mapstructure:"start"`
-	Help           string `mapstructure:"help"`
-	Location       string `mapstructure:"location"`
-	Time           string `mapstructure:"time"`
-	WrongTime      string `mapstructure:"wrong_time"`
-	SuccessfulTime string `mapstructure:"successful_time"`
-	UnknownCommand string `mapstructure:"unknown_command"`
-	DefaultMessage string `mapstructure:"default_message"`
-}
-
 type Config struct {
-	WeatherToken  string
-	TelegramToken string
-	MongoURI      string
+	WeatherToken  string `env:"WEATHER_TOKEN"`
+	TelegramToken string `env:"TELEGRAM_TOKEN"`
+	MongoURI      string `env:"MONGO_URI"`
+	LogFilePath   string `env:"LOG_FILE_PATH"`
 
-	Responses Responses
+	Response models.Response
 }
 
-func Init() (*Config, error) {
-	if err := setUpViper(); err != nil {
-		return nil, err
-	}
-
+func LoadConfig(logger *zap.SugaredLogger) (*Config, error) {
 	var cfg Config
 
-	if err := fromEnv(&cfg); err != nil {
+	if err := godotenv.Load(); err != nil {
+		logger.Errorf("unable to load env file: %s", err)
 		return nil, err
 	}
 
-	if err := unmarshal(&cfg); err != nil {
+	if err := env.Parse(&cfg); err != nil {
+		logger.Errorf("unable to parse config: %s", err)
+		return nil, err
+	}
+
+	if err := setUpViper(logger); err != nil {
+		return nil, err
+	}
+
+	if err := viper.UnmarshalKey("responses", &cfg.Response); err != nil {
+		logger.Errorf("faild to unmarshal responses: %s", err)
 		return nil, err
 	}
 
 	return &cfg, nil
 }
 
-func setUpViper() error {
-	viper.AddConfigPath("configs")
-	viper.SetConfigName("main")
+func setUpViper(logger *zap.SugaredLogger) error {
+	viper.AddConfigPath("../../pkg/config")
+	viper.SetConfigType("yml")
 
 	if err := viper.ReadInConfig(); err != nil {
-		log.Errorf("faild to load responses config file: %s", err)
+		logger.Errorf("unable to load config file: %s", err)
 		return err
 	}
-
-	return nil
-}
-
-func unmarshal(cfg *Config) error {
-	if err := viper.Unmarshal(&cfg); err != nil {
-		log.Errorf("error unmarshaling config into a struct: %s", err)
-		return err
-	}
-
-	if err := viper.UnmarshalKey("responses", &cfg); err != nil {
-		log.Errorf("error unmarshaling key into a struct: %s", err)
-		return err
-	}
-
-	return nil
-}
-
-func fromEnv(cfg *Config) error {
-	if err := viper.BindEnv("telegram_token"); err != nil {
-		log.Errorf("error binding a Viper key to a ENV variable: %s", err)
-		return err
-	}
-	cfg.TelegramToken = viper.GetString("telegram_token")
-
-	if err := viper.BindEnv("weather_token"); err != nil {
-		log.Errorf("error binding a Viper key to a ENV variable: %s", err)
-		return err
-	}
-	cfg.WeatherToken = viper.GetString("weather_token")
-
-	if err := viper.BindEnv("mongo_uri"); err != nil {
-		log.Errorf("error binding a Viper key to a ENV variable: %s", err)
-		return err
-	}
-	cfg.MongoURI = viper.GetString("mongo_uri")
 
 	return nil
 }
