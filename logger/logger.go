14476a980e4d7509c67fc5df1cca378cb56b02e4
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-04-06 17:57:05 +0200

    modified logger

--- logger/logger.go
+++ logger/logger.go
@@ -4,6 +4,8 @@ import (
 	"subscription-bot/config"
 
 	"go.uber.org/zap"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const (
@@ -13,26 +15,25 @@ const (
 
 func NewBotLogger(cfg *config.BotConfig) (*zap.SugaredLogger, error) {
 	var logger *zap.Logger
-	var sugar = logger.Sugar()
 	var err error
 
 	switch cfg.LogLevel {
 	case productionLevel:
 		logger, err = zap.NewProduction()
 		if err != nil {
-			sugar.With(err).Errorf("unable to build logger:\n'%v'", err)
+			log.Errorf("faild to build production logger:\n'%v'", err)
 			return nil, err
 		}
 	case developmentLevel:
 		logger, err = zap.NewDevelopment()
 		if err != nil {
-			sugar.With(err).Errorf("unable to build logger:\n'%v'", err)
+			log.Errorf("faild to build development logger:\n'%v'", err)
 			return nil, err
 		}
 	default:
 		logger, err = zap.NewDevelopment()
 		if err != nil {
-			sugar.With(err).Errorf("unable to build logger:\n'%v'", err)
+			log.Errorf("faild to build development logger:\n'%v'", err)
 			return nil, err
 		}
 	}
