f62cc6f14dfe553db4e2f1f93c0d72ee9276852d
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-04-06 12:27:00 +0200

    modified logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,51 +1,45 @@
 package logger
 
 import (
-	"os"
 	"subscription-bot/config"
 
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
-func NewLogger() (*zap.SugaredLogger, error) {
-	logger, err := zap.NewProduction()
+const (
+	developmentLevel = "DEBUG"
+	productionLevel  = "PRODUCTION"
+)
+
+func NewBotLogger(cfg *config.BotConfig) (*zap.SugaredLogger, error) {
+	var logger *zap.Logger
+	var err error
+
+	switch cfg.LogLevel {
+	case productionLevel:
+		logger, err = zap.NewProduction()
+		if err != nil {
+			s := logger.Sugar()
+			s.With(err).Errorf("unable to build logger:\n'%v'", err)
+			return nil, err
+		}
+	case developmentLevel:
+		logger, err = zap.NewDevelopment()
+		if err != nil {
+			s := logger.Sugar()
+			s.With(err).Errorf("unable to build logger:\n'%v'", err)
+			return nil, err
+		}
+	default:
+		logger, err = zap.NewDevelopment()
+		if err != nil {
+			s := logger.Sugar()
+			s.With(err).Errorf("unable to build logger:\n'%v'", err)
+			return nil, err
+		}
+	}
+
 	sugared := logger.Sugar()
 
-	if err != nil {
-		sugared.With(err).Errorf("unable to build logger: %s", err)
-		return nil, err
-	}
-
 	return sugared, nil
-}
-
-func LoadLogger(cfg *config.Config) (*zap.SugaredLogger, error) {
-	config := zap.NewProductionConfig()
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-
-	fileWriter, _, err := zap.Open(cfg.LogFilePath)
-	if err != nil {
-		return nil, err
-	}
-
-	fileWriteSyncer := zapcore.AddSync(fileWriter)
-
-	consoleWriteSyncer := zapcore.AddSync(os.Stdout)
-
-	writeSyncer := zapcore.NewMultiWriteSyncer(fileWriteSyncer, consoleWriteSyncer)
-
-	config.OutputPaths = []string{cfg.LogFilePath}
-	config.ErrorOutputPaths = []string{cfg.LogFilePath}
-	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-
-	core := zapcore.NewCore(
-		zapcore.NewConsoleEncoder(config.EncoderConfig),
-		writeSyncer,
-		zap.NewAtomicLevelAt(zap.InfoLevel),
-	)
-
-	logger := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-
-	return logger.Sugar(), nil
 }
