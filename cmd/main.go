bfe17db11b1a63c2f86632fc076a8959f6e60f0d
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-04-11 13:02:11 +0200

    modified main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,59 +1,15 @@
 package main
 
 import (
-	"subscription-bot/config"
-	"subscription-bot/container"
-	"subscription-bot/internal/api"
-	"subscription-bot/internal/bot"
-	"subscription-bot/internal/database"
-	"subscription-bot/logger"
-	_ "time/tzdata"
+	"subscription-bot/cmd/app"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	log "github.com/sirupsen/logrus"
+
+	_ "time/tzdata"
 )
 
 func main() {
-	cfg, err := config.LoadConfig()
-	if err != nil {
-		return
-	}
-
-	logger, err := logger.NewBotLogger(cfg)
-	if err != nil {
-		return
-	}
-
-	responses, err := config.LoadResponses(cfg, logger)
-	if err != nil {
-		return
-	}
-
-	container := container.NewBotContainer(cfg, logger)
-
-	forecastService := api.NewForecastService(container)
-
-	weatherService := api.NewWeatherService(forecastService)
-
-	mongoStorage := database.NewMongoStorageService(container, weatherService)
-
-	userStorage := database.NewUserStorageService(mongoStorage)
-
-	client, err := database.NewMongoClient(container)
-	if err != nil {
-		return
-	}
-
-	botApi, err := tgbotapi.NewBotAPI(container.GetConfig().TelegramToken)
-	if err != nil {
-		logger.Errorf("error creating new BotAPI:\n'%v'", err)
-		return
-	}
-	botApi.Debug = true
-
-	bot := bot.Newbot(botApi, client, userStorage, weatherService, container, responses)
-
-	if err := bot.Start(); err != nil {
-		logger.Errorf("unable to start bot:\n'%v'", err)
-		return
+	if err := app.Run(); err != nil {
+		log.Fatalf("Run(). Error: '%v'", err)
 	}
 }
