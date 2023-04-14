e64521fdcd4c6006c4c7ac0e8b320f711b8cd820
DESKTOP-CHBOR0U\Lenowo <oleh.bosykh@gmail.com>
2023-04-14 14:12:29 +0200

    modified config.go

--- config/config.go
+++ config/config.go
@@ -2,19 +2,25 @@ package config
 
 import (
 	"github.com/caarlos0/env"
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 )
 
-type BotConfig struct {
+type Config struct {
 	WeatherToken        string `env:"WEATHER_TOKEN"`
 	TelegramToken       string `env:"TELEGRAM_TOKEN"`
-	MongoURI            string `env:"MONGO_URI"`
-	LogLevel            string `env:"LOG_LEVEL"`
-	ResponsesConfigPath string `env:"RESPONSES_CONFIG_PATH"`
+	MongoURI            string `env:"MONGO_URI" envDefault:"mongodb://localhost:27017/"`
+	LogLevel            string `env:"LOG_LEVEL" envDefault:"DEBUG"`
+	ResponsesConfigPath string `env:"RESPONSES_CONFIG_PATH" envDefault:"../config"`
 }
 
-func LoadConfig() (*BotConfig, error) {
-	var cfg BotConfig
+func LoadConfig() (*Config, error) {
+	var cfg Config
+
+	if err := godotenv.Load(); err != nil {
+		log.Errorf("err load env: %s", err)
+		return nil, err
+	}
 
 	if err := env.Parse(&cfg); err != nil {
 		log.Errorf("unable to parse bot config: %s", err)
